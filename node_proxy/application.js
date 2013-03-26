1172c4db259e810e6397528d9283147c8eebeef4
Mehdi Hasan <mhasan@omicronlab.com>
2013-03-27 02:01:07 +0600

    Updated

--- node_proxy/application.js
+++ node_proxy/application.js
@@ -26,7 +26,16 @@ http.createServer(function (req, res) {
               console.log(body);
               
               var token = body.match(/access_token=([^&]*)/) ? body.match(/access_token=([^&]*)/)[1] : "";
-              res.writeHead(200);
+              
+              var headers = {};
+              headers["Access-Control-Allow-Origin"] = "*";
+              headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+              headers["Access-Control-Allow-Credentials"] = true;
+              headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+              headers["Access-Control-Allow-Headers"] = "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authorization, Accept";
+              
+              // respond to the request
+              res.writeHead(200, headers);
               res.end(token);
           }
       }
