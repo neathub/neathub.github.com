d1397f4e6f4a7f3e36f2f9f94efae6b863e4bd0f
Mehdi Hasan <mhasan@omicronlab.com>
2013-03-27 01:18:30 +0600

    Updated

---
+++ node_proxy/application.js
@@ -0,0 +1,35 @@
+var request = require('request');
+var http = require("http");
+var url = require("url");
+
+http.createServer(function (req, res) {
+  var code = url.parse(req.url, true).query.code;
+  
+  if (req.url === "/favicon.ico"){
+      res.writeHead(404);
+      res.end("\n");
+      return;
+  }
+  
+  var post_data = {
+      'code': code,
+      'client_id': "537693f51fcdd92eb755",
+      'client_secret': "10f913008f94e306428f71bcac9acebc76b07860",
+      'scope': "user,public_repo"
+  };
+  
+  request.post(
+      'https://github.com/login/oauth/access_token',
+      { form: post_data},
+      function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body);
+              
+              var token = body.match(/access_token=([^&]*)/) ? body.match(/access_token=([^&]*)/)[1] : "";
+              res.writeHead(200);
+              res.end(token);
+          }
+      }
+  );
+  
+}).listen(80);
